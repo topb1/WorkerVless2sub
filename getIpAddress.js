1ab16edf4212b7fecf267cd3cf20fc663f4c0301
lihaiquan <lihaiquan2009@126.com>
2024-12-24 22:34:00 +0800

    1

--- getIpAddress.js
+++ getIpAddress.js
@@ -1,11 +1,9 @@
 // https://github.com/cmliu/edgetunnel
 
 let sub = 'VLESS.fxxk.dedyn.io';// cmliu默认
-sub = 'alvless.comorg.us.kg';// 天诚优选
-// sub = 'moistr.freenods.sbs';// moistr优选
-// sub = '3k.fxxk.dedyn.io';// 第三方,3Kmfi6HP大佬维护
-
-
+// sub = 'alvless.comorg.us.kg';// 天诚优选
+sub = 'zrf.zrf.me';// 第三方,周润发维护
+sub = 'owo.o00o.ooo';// 第三方,Moist_R大佬维护
 
 
 let fakeHostName = 'ai.abcde.xyz'
@@ -17,7 +15,7 @@ let subProtocol = 'https';
 let url = `${subProtocol}://${sub}/sub?host=${fakeHostName}&uuid=${fakeUserID}&edgetunnel=cmliu&proxyip=${RproxyIP}`;
 url = `${subProtocol}://${subconverter}/sub?target=singbox&url=${encodeURIComponent(url)}&insert=false&config=${encodeURIComponent(subconfig)}&emoji=true&list=false&tfo=false&scv=true&fdn=false&sort=false&new_name=true`;
 
-// console.log(url);
+console.log(url);
 fetch(url)
   .then(response => response.json())
   .then(data => {
