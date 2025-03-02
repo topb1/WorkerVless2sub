abee4badbea6f34b52b0a1a5c27b8b46d424803b
lihaiquan <lihaiquan2009@126.com>
2025-03-02 22:09:11 +0800

    3.2

--- getIpAddress.js
+++ getIpAddress.js
@@ -2,8 +2,8 @@
 
 let sub = 'VLESS.fxxk.dedyn.io';// cmliu默认
 // sub = 'alvless.comorg.us.kg';// 天诚优选
-sub = 'zrf.zrf.me';// 第三方,周润发维护
-// sub = 'owo.o00o.ooo';// 第三方,Moist_R大佬维护
+// sub = 'zrf.zrf.me';// 第三方,周润发维护
+sub = 'owo.o00o.ooo';// 第三方,Moist_R大佬维护
 
 
 let fakeHostName = 'ai.abcde.xyz'
